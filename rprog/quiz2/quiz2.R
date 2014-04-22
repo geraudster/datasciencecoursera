fbf92be76f1fb27147bbc3aa03b4a77df6f881d4
geraud <geraudster@gmail.com>
2014-04-22 20:08:25 +0200

    end quiz2

--- rprog/quiz2/quiz2.R
+++ rprog/quiz2/quiz2.R
@@ -10,3 +10,10 @@ rowMeans(iris[,1:4])
 colMeans(iris[,1:4])
 apply(iris[,1:4], 2, mean)
 
+# Q3
+data(mtcars)
+lapply(split(mtcars$mpg, mtcars$cyl), mean)
+
+# Q4
+means <- lapply(split(mtcars$hp, mtcars$cyl), mean)
+abs(means$`4` - means$`8`)
