1a9cb707f7fbded25152c785a43cc3968a0f9278
Géraud Dugé <geraudster@gmail.com>
2016-03-11 15:55:49 +0100

    fix install script

--- docker/install-rstudio.sh
+++ docker/install-rstudio.sh
@@ -15,13 +15,12 @@ echo '**** Hacking locales...'
 apt-get -y install locales
 sed -i.bak 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen en_US.UTF-8
-LANG en_US.UTF-8
-LANGUAGE en_US:en
-LC_ALL en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+export LC_ALL=en_US.UTF-8
 
 echo '**** Launching big install...'
 apt-get -y install \
-    build-dep \
     curl \
     gdebi-core \
     git \
